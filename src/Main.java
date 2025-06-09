1aa2a03be18f36cbf1c0a280c2b6b2c143f05a78
solpi2 <parkchini@gmail.com>
2025-06-09 14:16:52 +0900

    String commit2

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,26 @@
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
         String message = "Hello, java!";
 
         // 문자열 길이 확인
-        System.out.println("문자열 길이: " + message.length());
+        // System.out.println("문자열 길이: " + message.length());
 
         // 대문자로 변환
-        System.out.println("대문자로 변환: " + message.toUpperCase());
+        // System.out.println("대문자로 변환: " + message.toUpperCase());
+
+        // 특정 문자열 추출
+        // System.out.println(message.indexOf("j"));
+
+
+        // 특정 문자열에서 멈춤
+        for (int i = 0; i < message.length(); i++) {
+
+            if(message.charAt(i) == 'l') {
+                System.out.println(message.charAt(i));
+                break;
+            }
+        }
     }
 }
