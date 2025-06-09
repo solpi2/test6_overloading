2bed9317ef4141ec8829a285f7c35fd1acb2e49c
solpi2 <parkchini@gmail.com>
2025-06-09 14:49:38 +0900

    String commit3 substring2

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,7 @@
-import java.sql.SQLOutput;
-
 public class Main {
     public static void main(String[] args) {
         String address = "0x120819f5573ac3A530e6172b1581613A09b4b338";
 
-        System.out.println(address.substring(0,7));
+        // System.out.println(address.substring(address.length() -5));
     }
 }
