67fdb4ad9e1bfafa2876f1e18e6c761d067b83c1
solpi2 <parkchini@gmail.com>
2025-06-09 10:28:24 +0900

    method overloading commit 2

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,14 @@
 public class Main {
-    public static int add(int a, int b) {
-        return a + b;
+    public static void greet() {
+        System.out.println("Hello, World!");
     }
 
-    public static int add(int a, int b, int c) {
-        return a + b + c;
+    public static void greet(String name) {
+        System.out.println("Hello, " + name + "!");
     }
 
     public static void main(String[] args) {
-        System.out.println("a + b = " + add(1, 2));
-        System.out.println("a + b + c = " + add(3,4,5));
+        greet();
+        greet("Alice");
     }
 }
