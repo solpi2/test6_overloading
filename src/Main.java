430aa4d611d68ee984fadbf85f87a7cf5a3a9aa7
solpi2 <parkchini@gmail.com>
2025-06-09 11:33:28 +0900

    Arrays.1

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,9 @@
 public class Main {
-    public static void greet() {
-        System.out.println("Hello, World!");
-    }
-
-    public static void greet(String name) {
-        System.out.println("Hello, " + name + "!");
-    }
-
     public static void main(String[] args) {
-        greet();
-        greet("Alice");
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7};
+
+        for (int i =0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
     }
 }
